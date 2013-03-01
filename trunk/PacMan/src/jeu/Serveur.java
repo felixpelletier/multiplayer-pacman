1c82d1fb255bb63c02f0ba400bb2c2c8b2a895b8
felixpelletier23 <felixpelletier23@0ca7d562-b07e-726f-ba94-2ee289747a36>
2013-03-01 04:21:01 +0000

    Premier layout de Niveau fait. Images corrigées

--- trunk/PacMan/src/jeu/Serveur.java
+++ trunk/PacMan/src/jeu/Serveur.java
@@ -1,5 +1,6 @@
 package jeu;
 
+import jeu.Case.TYPE_CASE;
 import affichage.FenetreJeu;
 
 public class Serveur {
@@ -7,8 +8,11 @@ public class Serveur {
 	private Case[][] Cases = new Case[Case.NB_CASES_X][Case.NB_CASES_Y];
 	
 	public Serveur(){
-		Cases[5][5] = new Case(Case.TYPE_CASE.BlocNW);
-		FenetreJeu fen = new FenetreJeu(Cases);
+		Niveau niv = new Niveau();
+		
+		FenetreJeu fen = new FenetreJeu(niv.getCases());
+		
+		System.out.println(niv.getNbPoint());
 		
 	}
 	
