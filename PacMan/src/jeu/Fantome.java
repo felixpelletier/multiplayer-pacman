3a82776de5f353519a4ce3812133a5224c1c9f5f
felixpelletier23 <felixpelletier23@0ca7d562-b07e-726f-ba94-2ee289747a36>
2013-03-08 14:51:38 +0000

    Commencé Fantome

--- PacMan/src/jeu/Fantome.java
+++ PacMan/src/jeu/Fantome.java
@@ -3,8 +3,36 @@ package jeu;
 import java.awt.Point;
 
 public abstract class Fantome extends Personnage{
+	
+	protected static Case[][] cases;
+	protected static Pacman joueur;
 
-	public Fantome(Case[][] cases) {
+	public enum MODE{
+		Disperser,Chasser;
+	}
+	
+	private static final MODE[] listeMode = {	MODE.Disperser,MODE.Chasser,
+												MODE.Disperser,MODE.Chasser,
+												MODE.Disperser,MODE.Chasser,
+												MODE.Disperser};
+	
+	private static final float[] tempsMode = {	7,20,
+												7,20,
+												5,20,
+												5};
+	
+	private int etape = 0;
+	
+	public static void initialiser(Case[][] cases, Pacman joueur){
+		Fantome.cases = cases;
+		Fantome.joueur = joueur;
+	}
+	
+	public MODE getMode(){
+		return MODE.Chasser;
+	}
+	
+	public Fantome() {
 		super(cases);
 	}
 	
