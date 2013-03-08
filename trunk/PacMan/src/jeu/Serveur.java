7bce46328ca5c5c60dfffa2360be3711cd6dd351
felixpelletier23 <felixpelletier23@0ca7d562-b07e-726f-ba94-2ee289747a36>
2013-03-08 14:51:38 +0000

    Commencé Fantome

--- trunk/PacMan/src/jeu/Serveur.java
+++ trunk/PacMan/src/jeu/Serveur.java
@@ -4,20 +4,26 @@ import jeu.Case.TYPE_CASE;
 import affichage.FenetreJeu;
 
 public class Serveur {
-
-	private Case[][] Cases = new Case[Case.NB_CASES_X][Case.NB_CASES_Y];
+	
+	private Case[][] cases = new Case[Case.NB_CASES_X][Case.NB_CASES_Y];
 	private int compteurPoints;
+	public static long frame = 0;
+	public static int FRAMERATE = 30;
 	
 	public Serveur(){
 		Niveau niv = new Niveau();
 		
 		compteurPoints = niv.getNbPoint();
 		
-		Cases = niv.getCases();
+		cases = niv.getCases();
 		
-		Pacman j = new Pacman(Cases);
+		Pacman j = new Pacman(cases);
 		
-		FenetreJeu fen = new FenetreJeu(Cases);
+		Fantome.initialiser(cases, j);
+	
+		
+		
+		FenetreJeu fen = new FenetreJeu(cases);
 		
 		fen.addChose(j);
 		fen.addKeyListener(j);
@@ -28,12 +34,18 @@ public class Serveur {
 			
 			fen.repaint();
 			
+			frame++;
+			
 			//Cases[1][6] = new Case(TYPE_CASE.Vide);
 			
 			try {
-	            Thread.sleep(1000 / 30);  // milliseconds
+	            Thread.sleep(1000 / FRAMERATE);  // milliseconds
 	         } catch (InterruptedException ex) { }
 		}
+	}
+	
+	public static int getSecondes(){
+		return (int) (frame/FRAMERATE);
 	}
 	
 	public static void main(String[] args) {
